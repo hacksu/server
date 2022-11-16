fad6ccf4e4fe294649ac4f22ee1f56d704891027
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-11-16 18:32:46 -0500

    more stuff

--- scripts/setup/mongodb.sh
+++ scripts/setup/mongodb.sh
@@ -4,20 +4,22 @@
 CWD=$( pwd );
 REPO=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && git rev-parse --show-toplevel );
 
+MONGO_VERSION=4.4
+
 if ! [ -x "$(command -v mongo)" ]; then
     echo "Installing MongoDB";
     source $REPO/scripts/setup/env.sh
 #   fetch package registry
-    sudo apt update -y
+    sudo apt-get update -y
 #   install gnupg
-    sudo apt install gnupg -y
+    sudo apt-get install gnupg -y
 #   add mongodb pgp key
-    wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
-    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+    wget -qO - https://www.mongodb.org/static/pgp/server-$MONGO_VERSION.asc | sudo apt-key add -
+    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/$MONGO_VERSION multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-$MONGO_VERSION.list
 #   fetch package registry
-    sudo apt update -y
+    sudo apt-get update -y
 #   install mongodb
-    sudo apt install -y mongodb-org
+    sudo apt-get install -y mongodb-org
 #   start mongodb
     systemctl start mongod.service
 #   enable mongodb on restart
