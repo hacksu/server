f6bd0f7d1f2604994879434541a0aa470257ba70
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-11-16 16:56:19 -0500

    another change

--- scripts/setup/node.sh
+++ scripts/setup/node.sh
@@ -4,17 +4,14 @@
 CWD=$( pwd );
 SWD=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd );
 
-# Prevents interactive prompts from running during a command
-NI = "DEBIAN_FRONTEND=noninteractive";
-
 if [ -x "$(command -v node)" ]; then
     echo "Node.js $(node -v) is already installed";
 else
     echo "Installing Node.js & NPM";
 #   fetch package registry
-    $NI sudo apt update -y
+    DEBIAN_FRONTEND=noninteractive sudo apt update -y
 #   install node & npm
-    $NI sudo apt install -y nodejs npm
+    DEBIAN_FRONTEND=noninteractive sudo apt install -y nodejs npm
 #   install n to allow us to easily swap between nodejs versions
     npm install --global n
 #   swap to node v18
