564fba3fa8790457dcdd540b21e09a9a65a28b13
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-11-16 18:38:42 -0500

    another

--- scripts/setup/mongodb.sh
+++ scripts/setup/mongodb.sh
@@ -9,19 +9,15 @@ MONGO_VERSION=4.4
 if ! [ -x "$(command -v mongo)" ]; then
     echo "Installing MongoDB";
     source $REPO/scripts/setup/env.sh
-#   fetch package registry
-    sudo apt-get update -y
 #   add ubuntu 20.xx (focal) sources
-    echo "deb http://security.ubuntu.com/ubuntu focal-security main" | sudo tee /etc/apt/sources.list.d/focal-security.lis
-#   install gnupg
-    sudo apt-get install gnupg libssl1.1 -y
+    echo "deb http://security.ubuntu.com/ubuntu focal-security main" | sudo tee /etc/apt/sources.list.d/focal-security.list
 #   add mongodb pgp key
-#    wget -qO - https://www.mongodb.org/static/pgp/server-$MONGO_VERSION.asc | sudo apt-key add -
     curl -fsSL https://www.mongodb.org/static/pgp/server-$MONGO_VERSION.asc | sudo apt-key add -
-#    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/$MONGO_VERSION multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-$MONGO_VERSION.list
     echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/$MONGO_VERSION multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-$MONGO_VERSION.list
 #   fetch package registry
     sudo apt-get update -y
+#   install gnupg
+    sudo apt-get install -y gnupg libssl1.1
 #   install mongodb
     sudo apt-get install -y mongodb-org
 #   start mongodb
