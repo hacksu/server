94bd6683167d83206a5b5b58549a3ab75b396341
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-11-16 18:13:29 -0500

    nginx script

--- scripts/setup/env.sh
+++ scripts/setup/env.sh
@@ -3,3 +3,6 @@
 # Ensure needsrestart is set to automatic
 # https://stackoverflow.com/questions/73397110/how-to-stop-ubuntu-pop-up-daemons-using-outdated-libraries-when-using-apt-to-i
 sed -i "/#\$nrconf{restart} = 'i';/c\$nrconf{restart} = 'a';" /etc/needrestart/needrestart.conf;
+
+# Allow SSH through the firewall
+ufw allow 'OpenSSH'
---
+++ scripts/setup/nginx.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Environment Variables
+CWD=$( pwd );
+REPO=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && git rev-parse --show-toplevel );
+
+if ! [ -x "$(command -v nginx)" ]; then
+    echo "Installing NGINX";
+    source $REPO/scripts/setup/env.sh
+#   fetch package registry
+    sudo apt update -y
+#   install node & npm
+    sudo apt install -y nginx
+#   allow it through firewall
+    ufw allow "Nginx Full"
+#   enable firewall
+    ufw enable
+#   firewall status
+    ufw status
+fi
+
+# get nginx status
+systemctl status nginx
