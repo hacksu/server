0730fa513750b9f70c0468558e5d8009b70b94fb
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2023-01-18 18:13:25 -0500

    eey

--- projects/teams-ssh/setup.sh
+++ projects/teams-ssh/setup.sh
@@ -7,8 +7,7 @@ REPO=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && git rev-pars
 cd $REPO/projects/teams-ssh
 bash $REPO/scripts/setup/pm2.sh
 
-npm run test
-pm2 start --name teams-ssh "npm run start"
-pm2 save
+npm run test && pm2 start --name teams-ssh "npm run start" && pm2 save
+
 
 
