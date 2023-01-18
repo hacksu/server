23363102e8537570ae5c1a11ffa91d659eed94be
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2023-01-18 18:19:59 -0500

    noice error

--- projects/teams-ssh/src/config.ts
+++ projects/teams-ssh/src/config.ts
@@ -1,8 +1,10 @@
 import { cleanEnv, num, str } from 'envalid';
 import { App } from '@octokit/app';
-import { mkdirSync, readFileSync } from 'fs';
+import { existsSync, mkdirSync, readFileSync } from 'fs';
 import { resolve } from 'path';
 import dotenv from 'dotenv';
+import { EOL } from 'os';
+import chalk from 'chalk';
 
 dotenv.config({
     override: true,
@@ -29,9 +31,23 @@ mkdirSync(__data, {
     recursive: true
 })
 
+const __appKey = resolve(process.cwd(), env.GITHUB_APP_KEY);
+if (!existsSync(__appKey)) {
+    console.error();
+    console.error(chalk.red(`Missing Github App Private Key`));
+    console.error();
+    console.error([
+        `Ensure ${chalk.grey(__appKey)} is present`,
+        `This is a private key tied to ${chalk.blue('https://github.com/organizations/hacksu/settings/apps/hacksu-read')}`,
+        `You can generate a new private key by clicking  ${chalk.bgGray(' Generate a private key ')} on the page linked above`,
+    ].map(o => ' - ' + o).join(EOL));
+    console.error();
+    process.exit(1);
+}
+
 export const app = new App({
     appId: env.GITHUB_APP_ID,
-    privateKey: readFileSync(resolve(process.cwd(), env.GITHUB_APP_KEY), 'utf8'),
+    privateKey: readFileSync(__appKey, 'utf8'),
 });
 
 export const installation = app.getInstallationOctokit(env.GITHUB_INSTALL_ID)
