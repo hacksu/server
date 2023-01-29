bc5555034fed3776e9b034ee7aff6c6385f57e4e
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2023-01-29 14:05:35 -0500

    a

--- projects/teams-ssh/src/zsh.ts
+++ projects/teams-ssh/src/zsh.ts
@@ -3,7 +3,11 @@ import { readFile, writeFile } from 'fs/promises';
 
 const theme = readFile(process.cwd() + '/../../scripts/config/robbyrussel.zsh.theme');
 export async function updateZshTheme(user: string) {
-    const __zsh = user === 'root' ? '/root/.oh-my-zsh' : `/home/${user}/.oh-my-zsh`;
-    await writeFile(__zsh + '/themes/robbyrussell.zsh-theme', await theme);
+    try {
+        const __zsh = user === 'root' ? '/root/.oh-my-zsh' : `/home/${user}/.oh-my-zsh`;
+        await writeFile(__zsh + '/themes/robbyrussell.zsh-theme', await theme);
+    } catch (err) {
+        console.error(`failed to update zsh for ${user}`, err);
+    }
 }
 
